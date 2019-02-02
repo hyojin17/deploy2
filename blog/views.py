3c5f0ea3d9aec5e12c6615c1e87259783b7a6f15
hyojin17 <chj7960@likelion.org>
2019-02-02 20:08:14 +0900

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 #모델의 Blog를 끌어온다.
 
@@ -7,5 +7,7 @@ def home(request):
     #쿼리셋-> 객체들의 목록을 전달받을 수 있다(.objects)#메소드(쿼리셋을 정렬할 수 있게하거나 기능하게 해주는 방법중 하나)
     return render(request, 'home.html', {'blogs': blogs})
 
-    #쿼리셋과 메소드의 형식
-    #모델.쿼리셋(objects).메소드
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk = blog_id)#blog_id번째 블로그 객체
+    #첫번째 객체는 클래스이름, 두번째객체는 pk값
+    return render(request, 'detail.html', {'blog':blog_detail})
